a81b9fe159dd1568e99c5fb89e628302ba4937d9
HongRin <72390797+HongRin@users.noreply.github.com>
2021-06-10 12:32:49 +0900

    ObjectPool_Implemented
    ObjectPool 구현

--- Source/AssignmentGame/Util/ObjectPool.cpp
+++ Source/AssignmentGame/Util/ObjectPool.cpp
@@ -1,2 +1,29 @@
 #include "ObjectPool.h"
 
+IObjectPoolable* UObjectPool::GetRecycledObject(int32 objId)
+{
+	// 요소의 개수가 0 이라면 리턴합니다.
+	if (PoolObjects.Num() == 0) return nullptr;
+
+	// 새로운 오브젝트를 생성합니다.
+	IObjectPoolable* poolableObj = nullptr;
+
+	// 요소의 개수만큼 반복합니다.
+	for (auto elem : PoolObjects)
+	{
+		// 재사용이 가능하며 등록된 아이디가 같다면
+		if (elem->GetCanRecyclable() && elem->GetID() == objId)
+		{
+			// 오브젝트를 등록합니다.
+			(poolableObj = elem)->SetCanRecyclable(false);
+			poolableObj->OnRecycleStart();
+			break;
+		}
+	}
+
+	// 반복문이 끝난 후에도 찾지 못했다면 리턴합니다.
+	if (poolableObj == nullptr)
+		return nullptr;
+
+	return poolableObj;
+}
--- Source/AssignmentGame/Util/ObjectPool.h
+++ Source/AssignmentGame/Util/ObjectPool.h
@@ -10,4 +10,20 @@ class ASSIGNMENTGAME_API UObjectPool : public UObject
 {
 	GENERATED_BODY()
 
+private :
+	// 풀링할 오브젝트들을 저장할 배열
+	TArray<IObjectPoolable*> PoolObjects;
+
+public :
+	// 새로운 오브젝트를 등록합니다. 
+	template<typename T>
+	T* RegisterRecyclableObject(T* newRecyclableObject)
+	{
+		PoolObjects.Add((IObjectPoolable*)newRecyclableObject);
+		return newRecyclableObject;
+	}
+
+	// 재사용된 객체를 얻습니다.
+	IObjectPoolable* GetRecycledObject(int32 objId);
+
 };
