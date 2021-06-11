1daecab0d678f59af1063457dce5e9a263a57369
HongRin <72390797+HongRin@users.noreply.github.com>
2021-06-11 22:07:34 +0900

    DungeonList_Update
    코드에 따라 던전 정보 업데이트

--- Source/AssignmentGame/Widgets/TitleWidget/Dungeon/DungeonWnd.cpp
+++ Source/AssignmentGame/Widgets/TitleWidget/Dungeon/DungeonWnd.cpp
@@ -1,5 +1,42 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "DungeonWnd.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
 
+#include "Single/GameInstance/AGGameInst.h"
+#include "Single/PlayerManager/PlayerManager.h"
+
+
+UDungeonWnd::UDungeonWnd(const FObjectInitializer& ObjInitializer) :
+	Super(ObjInitializer)
+{
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_DUNGEON_INFO(
+		TEXT("DataTable'/Game/DataTable/DT_DungeonInfo.DT_DungeonInfo'"));
+	if (DT_DUNGEON_INFO.Succeeded()) DT_DungeonInfo = DT_DUNGEON_INFO.Object;
+}
+
+void UDungeonWnd::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	InitializeDungeonWnd();
+
+	Button_Select->OnClicked.AddDynamic(this, &UDungeonWnd::OnClickButtonSelect);
+}
+
+void UDungeonWnd::InitializeDungeonWnd()
+{
+	FString contextString;
+	DungeonInfo = DT_DungeonInfo->FindRow<FDungeonInfo>(Code, contextString);
+
+	Text_Name->SetText(DungeonInfo->DungeonName);
+	
+	UTexture2D* dungeonImage =
+		Cast<UTexture2D>(GetManager(FStreamableManager)->LoadSynchronous(DungeonInfo->DungeonImagePath));
+
+	Image_Sprite->SetBrushFromTexture(dungeonImage);
+}
+
+void UDungeonWnd::OnClickButtonSelect()
+{
+}
--- Source/AssignmentGame/Widgets/TitleWidget/Dungeon/DungeonWnd.h
+++ Source/AssignmentGame/Widgets/TitleWidget/Dungeon/DungeonWnd.h
@@ -1,17 +1,43 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
-#include "CoreMinimal.h"
+#include "AssignmentGame.h"
 #include "Blueprint/UserWidget.h"
+#include "Structures/DungeonInfo/DungeonInfo.h"
 #include "DungeonWnd.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class ASSIGNMENTGAME_API UDungeonWnd : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dungeon Code")
+	FName Code;
+
+private :
+	class UDataTable* DT_DungeonInfo;
+	FDungeonInfo* DungeonInfo;
+
+private :
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* Text_Name;
 	
+	UPROPERTY(meta = (BindWidget))
+	class UImage* Image_Sprite;
+	
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Button_Select;
+
+public :
+	UDungeonWnd(const FObjectInitializer& ObjInitializer);
+
+protected:
+	virtual void NativeConstruct() override;
+
+private :
+	void InitializeDungeonWnd();
+
+public :
+	UFUNCTION()
+	void OnClickButtonSelect();
 };
